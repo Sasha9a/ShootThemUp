bb72d16b8d12fec6e7a2d137c7c901de47a8d9cd
Sasha9a <aleksandr_dovigyy@mail.ru>
2022-08-21 17:55:28 +0300

    Заливка

---
+++ Source/ShootThemUp/Public/Player/STUBaseCharacter.h
@@ -0,0 +1,51 @@
+// Shoot Them Up Game, All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Camera/CameraComponent.h"
+#include "GameFramework/Character.h"
+#include "GameFramework/SpringArmComponent.h"
+#include "STUBaseCharacter.generated.h"
+
+UCLASS()
+class SHOOTTHEMUP_API ASTUBaseCharacter : public ACharacter {
+	GENERATED_BODY()
+
+public:
+	// Sets default values for this character's properties
+	ASTUBaseCharacter(const FObjectInitializer& ObjInit);
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadWrite, Category = "Components")
+	UCameraComponent* CameraComponent;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadWrite, Category = "Components")
+	USpringArmComponent* SpringArmComponent;
+
+public:
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+	// Called to bind functionality to input
+	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+
+	UFUNCTION(BlueprintCallable, Category = "Movement")
+	bool IsRunning() const;
+
+	UFUNCTION(BlueprintCallable, Category = "Movement")
+	float GetMovementDirection() const;
+
+private:
+	bool WantsToRun = false;
+	bool IsMovingForward = false;
+	
+	void MoveForward(float Amount);
+	void MoveRight(float Amount);
+	
+	void OnStartRunning();
+	void OnStopRunning();
+};
