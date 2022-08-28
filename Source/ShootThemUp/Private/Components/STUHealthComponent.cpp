3386fb7bd9471d66d149fa2f0550a87d9aad3a8d
Sasha9a <aleksandr_dovigyy@mail.ru>
2022-08-28 14:29:34 +0300

    Урок 39 Восстановление здоровья

--- Source/ShootThemUp/Private/Components/STUHealthComponent.cpp
+++ Source/ShootThemUp/Private/Components/STUHealthComponent.cpp
@@ -15,8 +15,7 @@ void USTUHealthComponent::BeginPlay()
 {
 	Super::BeginPlay();
 
-	Health = MaxHealth;
-	OnHealthChanged.Broadcast(Health);
+	SetHealth(MaxHealth);
 
 	AActor* ComponentOwner = GetOwner();
 	if (ComponentOwner) {
@@ -26,12 +25,28 @@ void USTUHealthComponent::BeginPlay()
 
 void USTUHealthComponent::OnTakeAnyDamage(AActor* DamagedActor, float Damage, const UDamageType* DamageType, AController* InstigatedBy,
 		AActor* DamageCauser) {
-	if (Damage <= 0.0f || IsDead()) return;
-	
-	Health = FMath::Clamp(Health - Damage, 0.0f, MaxHealth);
-	OnHealthChanged.Broadcast(Health);
+	if (Damage <= 0.0f || IsDead() || !GetWorld()) return;
+
+	SetHealth(Health - Damage);
+	GetWorld()->GetTimerManager().ClearTimer(HealTimerHandle);
 
 	if (IsDead()) {
 		OnDeath.Broadcast();
+	} else if (AutoHeal) {
+		GetWorld()->GetTimerManager().SetTimer(HealTimerHandle, this, &USTUHealthComponent::HealUpdate,
+			HealUpdateTime, true, HealDelay);
 	}
+}
+
+void USTUHealthComponent::HealUpdate() {
+	SetHealth(Health + HealModifier);
+
+	if (FMath::IsNearlyEqual(Health, MaxHealth) && GetWorld()) {
+		GetWorld()->GetTimerManager().ClearTimer(HealTimerHandle);
+	}
+}
+
+void USTUHealthComponent::SetHealth(float NewHealth) {
+	Health = FMath::Clamp(NewHealth, 0.0f, MaxHealth);
+	OnHealthChanged.Broadcast(Health);
 }
