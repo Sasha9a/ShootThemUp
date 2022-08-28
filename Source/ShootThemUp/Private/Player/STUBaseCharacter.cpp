c8a9d3aafb98f2cd14948fb40e70c9e904a0d533
Sasha9a <aleksandr_dovigyy@mail.ru>
2022-08-28 15:12:04 +0300

    Урок 41 Рефакторинг

--- Source/ShootThemUp/Private/Player/STUBaseCharacter.cpp
+++ Source/ShootThemUp/Private/Player/STUBaseCharacter.cpp
@@ -52,6 +52,7 @@ void ASTUBaseCharacter::Tick(float DeltaTime) {
 // Called to bind functionality to input
 void ASTUBaseCharacter::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent) {
 	Super::SetupPlayerInputComponent(PlayerInputComponent);
+	check(PlayerInputComponent);
 
 	if (PlayerInputComponent) {
 		PlayerInputComponent->BindAxis("MoveForward", this, &ASTUBaseCharacter::MoveForward);
@@ -112,7 +113,7 @@ void ASTUBaseCharacter::OnHealthChanged(float Health) {
 }
 
 void ASTUBaseCharacter::OnGroundLanded(const FHitResult& Hit) {
-	const auto FallVelocityZ = -GetCharacterMovement()->Velocity.Z;
+	const auto FallVelocityZ = -GetVelocity().Z;
 	UE_LOG(BaseCharacterLog, Display, TEXT("On landed: %f"), FallVelocityZ);
 
 	if (FallVelocityZ < LandedDamageVelocity.X) return;
